faf57438ec9c0f4511f7c441020a26ec634a15f8
Erkang Xia <xiaerkang@icloud.com>
2021-12-12 17:13:39 -0500

    Initial Commit

---
+++ StockNotes/StockNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockNotesApp.swift
+//  StockNotes
+//
+//  Created by Erkang Xia on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct StockNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
